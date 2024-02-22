b8a14290b1926eb331bb19653530e4b811eb4b56
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2024-02-22 20:41:29 -0300

    middleware to commonjs

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const auth = require("../config/firebase");
+
+const validateFirebaseToken = async (req, res, next) => {
+  
+    const token = req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const decodedToken = await auth.verifyIdToken(token);
+    res.locals.user = decodedToken.uid;
+    next();
+  } catch (error) {
+    console.error("Error verifying Firebase token:", error);
+    return res.status(403).json({ error: "Unauthorized!" });
+  }
+  next();
+};
+
+module.exports = validateFirebaseToken;
