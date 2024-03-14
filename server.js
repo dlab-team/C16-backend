2d50f4e12395b570b1ae5299d919bec493066878
Felipe <felipe.guzman102@hotmail.com>
2024-03-14 19:32:02 -0300

    added new routes + cors to main server file

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 const resourcesRouter = require("./routes/resources");
 const mailRouter = require("./routes/mail");
+const postsRouter = require("./routes/posts");
+const commentsRouter = require("./routes/comments");
 const { sequelize } = require("./config/database");
 
 /*TODO: 
--SETUP CORS 
 -SETUP firebase SDK
 */
-
+const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", resourcesRouter);
 app.use("/api", mailRouter);
+app.use("/api", postsRouter);
+app.use("/api", commentsRouter);
 
 app.get("/", (req, res) => {
-  res.send(
-    `<a href="http://localhost:${PORT}/api/resources">/api/resources</a>`
-  );
+  const baseUrl = req.protocol + "://" + req.get("host");
+  res.json({
+    resources: baseUrl + "/api/resources",
+    posts: baseUrl + "/api/posts",
+  });
 });
 
 sequelize.sync().then(() => {
