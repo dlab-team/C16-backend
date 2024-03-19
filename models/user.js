580ef3295b943e259b89327dcc827775d0e2d352
Felipe Burboa <felipeburboa1991@gmail.com>
2024-03-19 01:51:20 -0300

    fix: fixing user atributtes, pagination and search condition

--- models/user.js
+++ models/user.js
@@ -3,16 +3,18 @@ const { sequelize, Sequelize } = require("../config/database");
 const User = sequelize.define(
     'User', {
     id: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
         allowNull: false,
         unique: true,
         primaryKey: true
     },
     firstname: {
         type: Sequelize.STRING,
+        allowNull: true
     },
     lastname: {
         type: Sequelize.STRING,
+        allowNull: true
     },
     email: {
         type: Sequelize.STRING,
@@ -21,32 +23,47 @@ const User = sequelize.define(
     },
     phone: {
         type: Sequelize.STRING,
+        allowNull: true
     },
     rut: {
         type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
     },
     birthday: {
-        type: Sequelize.STRING,
+        type: Sequelize.DATE,
+        allowNull: true
     },
     gender: {
         type: Sequelize.STRING,
+        allowNull: true
     },
     region: {
         type: Sequelize.STRING,
+        allowNull: true
     },
     comuna: {
         type: Sequelize.STRING,
+        allowNull: true
     },
     takesCare: {
         type: Sequelize.STRING,
+        allowNull: true
     },
     photo: {
         type: Sequelize.STRING,
+        allowNull: true
     },
     completed: {
         type: Sequelize.BOOLEAN,
-        defaultValue:  false
+        allowNull: false,
+        defaultValue:  0
     }
-});
+},
+    {
+        timestamps: true,
+        createdAt: "createdAt",
+        updatedAt: "updatedAt",
+    });
 
 module.exports = User; 
