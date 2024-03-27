7226cc779835c15586ee6e5b0e3feb6f9d291c4c
Felipe Guzmán <44147985+Ks207@users.noreply.github.com>
2024-03-27 17:32:46 -0300

    Added more methos for corsOptions

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 const corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
